6a242e093ff2b165a2aade4a55064e147c0413fc
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-01 21:50:17 +0530

    Added error check

--- script.js
+++ script.js
@@ -25,57 +25,65 @@ function playGame() {
     let numberofGames = 5;
 
     function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase()
-        computerChoice = computerChoice.toLowerCase()
-        console.log(`Human selected ${humanChoice} and Comp generated ${computerChoice}`)
-    
-        switch(humanChoice) {
-            case 'paper' : if (computerChoice == 'scissors') {
-                console.log('You lose! Scissors beat paper')
-                console.log(`Scores: Human: ${humanScore} and Computer: ${++computerScore}`) ;
-                
-                }
-                else if (computerChoice == 'rock') {
-                    console.log('You win! Paper beats rock')
-                    console.log(`Scores: Human: ${++humanScore} and Computer: ${computerScore}`) ;
-                }
-                else if (computerChoice == 'paper') {
-                    console.log('It is a tie!')
-                    console.log(`Scores: Human: ${humanScore} and Computer: ${computerScore}`) ;   
-                }
-            break;
-            
-            case 'scissors' : if (computerChoice == 'rock') {
-                console.log('You lose! Rock beats scissors')
-                console.log(`Scores: Human: ${humanScore} and Computer: ${++computerScore}`) ;
-                
-                }
-                else if (computerChoice == 'paper') {
-                    console.log('You win! Scissors beats paper')
-                    console.log(`Scores: Human: ${++humanScore} and Computer: ${computerScore}`) ;
-                }
-                else if (computerChoice == 'scissors') {
-                    console.log('It is a tie!')
-                    console.log(`Scores: Human: ${humanScore} and Computer: ${computerScore}`) ;   
-                }
-            break;
-            
-            case 'rock' : if (computerChoice == 'paper') {
-                console.log('You lose! Paper beats rock')
-                console.log(`Scores: Human: ${humanScore} and Computer: ${++computerScore}`) ;
-                
-                }
-                else if (computerChoice == 'scissors') {
-                    console.log('You win! Rock beats scissors')
-                    console.log(`Scores: Human: ${++humanScore} and Computer: ${computerScore}`) ;
-                }
-                else if (computerChoice == 'rock') {
-                    console.log('It is a tie!')
-                    console.log(`Scores: Human: ${humanScore} and Computer: ${computerScore}`) ;   
-                }
-            break;
-
+        
+        if (humanChoice === null) {
+            // No chouice given, assume 'rock'
+            console.log('Please enter a choice');
         }
+        else {
+            humanChoice = humanChoice.toLowerCase()
+            computerChoice = computerChoice.toLowerCase()
+            
+            console.log(`Human selected ${humanChoice} and Comp generated ${computerChoice}`)
+        
+            switch(humanChoice) {
+                case 'paper' : if (computerChoice == 'scissors') {
+                    console.log('You lose! Scissors beat paper')
+                    console.log(`Scores: Human: ${humanScore} and Computer: ${++computerScore}`) ;
+                    
+                    }
+                    else if (computerChoice == 'rock') {
+                        console.log('You win! Paper beats rock')
+                        console.log(`Scores: Human: ${++humanScore} and Computer: ${computerScore}`) ;
+                    }
+                    else if (computerChoice == 'paper') {
+                        console.log('It is a tie!')
+                        console.log(`Scores: Human: ${humanScore} and Computer: ${computerScore}`) ;   
+                    }
+                break;
+                
+                case 'scissors' : if (computerChoice == 'rock') {
+                    console.log('You lose! Rock beats scissors')
+                    console.log(`Scores: Human: ${humanScore} and Computer: ${++computerScore}`) ;
+                    
+                    }
+                    else if (computerChoice == 'paper') {
+                        console.log('You win! Scissors beats paper')
+                        console.log(`Scores: Human: ${++humanScore} and Computer: ${computerScore}`) ;
+                    }
+                    else if (computerChoice == 'scissors') {
+                        console.log('It is a tie!')
+                        console.log(`Scores: Human: ${humanScore} and Computer: ${computerScore}`) ;   
+                    }
+                break;
+                
+                case 'rock' : if (computerChoice == 'paper') {
+                    console.log('You lose! Paper beats rock')
+                    console.log(`Scores: Human: ${humanScore} and Computer: ${++computerScore}`) ;
+                    
+                    }
+                    else if (computerChoice == 'scissors') {
+                        console.log('You win! Rock beats scissors')
+                        console.log(`Scores: Human: ${++humanScore} and Computer: ${computerScore}`) ;
+                    }
+                    else if (computerChoice == 'rock') {
+                        console.log('It is a tie!')
+                        console.log(`Scores: Human: ${humanScore} and Computer: ${computerScore}`) ;   
+                    }
+                break;
+
+            }
+        }    
     }
 
     for(let i = 0; i < numberofGames; i++) {
